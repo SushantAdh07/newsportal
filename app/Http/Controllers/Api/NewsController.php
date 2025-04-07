c56ff222c858e062fef1bd14a810e3b1ceaba57c
sushantadh07 <sushantadhikari2828@gmail.com>
2025-04-07 23:30:25 +0545

    api-2

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -9,18 +9,27 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    public function index(){
-        $news = News::get();
+    public function index(Request $request){
+
+        $query = News::query();
+
+        if ($request->has('name')) {
+            $query->where('name', 'like', '%' . $request->name . '%');
+        }
+
+        return response()->json($query);
+
+
+        /*$news = News::get();
         if($news->count() > 0){
             return NewsResource::collection($news);
         }
         else{
             return response()->json(['message' => 'No news available']);
-        }
+        } */
     }
 
     public function store(){
-
     }
 
     public function show(){
