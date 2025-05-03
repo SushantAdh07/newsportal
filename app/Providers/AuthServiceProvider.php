0e6295e07f6966f2c93275b9aa18229c66c7aa1b
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-05-03 22:34:51 +0545

    defining gates

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +28,16 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Gate::define('admin-access', function(User $user){
+            return $user->role === 'admin';
+        });
+
+        Gate::define('user-access', function(User $user){
+            return $user->role === 'user';
+        });
+
+        Gate::define('editor-access', function(User $user){
+            return $user->role === 'editor';
+        });
     }
 }
