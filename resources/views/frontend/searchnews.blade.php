f047e91946cc7a9c32f82c3387f4d698e5a7ba87
Sushantadh07 <sushantadhikari2828@gmail.com>
2025-05-14 16:45:43 +0545

    search page, added data to news table

--- resources/views/frontend/searchnews.blade.php
+++ resources/views/frontend/searchnews.blade.php
@@ -2,13 +2,50 @@
 
 @section('news')
     @if (count($results) > 0)
-        <ul>
-            @foreach ($results as $result)
-                <li>
-                    <a href="{{ route('details', $result->id) }} ">{{ $result->news_title }}</a>
-                </li>
-            @endforeach
-        </ul>
+        <section class="themesBazar_section_one">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-9 col-md-8">
+
+                            <div class="sec-one-item2">
+                                <div class="row">
+                                    @foreach ($results as $item)
+                                        <div class="themesBazar-3 themesBazar-m2">
+
+                                            <div class="sec-one-wrpp2">
+
+                                                <div class="secOne-news">
+                                                    <div class="secOne-image2">
+                                                        <a href="{{ route('details', $item->id) }} "><img class="lazyload"
+                                                                src="{{ asset($item->image) }}"></a>
+                                                    </div>
+                                                    <h4 class="secOne-title2">
+                                                        <a
+                                                            href="{{ route('details', $item->id) }} ">{{ $item->news_title }}</a>
+                                                    </h4>
+                                                </div>
+                                                <div class="cat-meta">
+                                                    <a href=" "> <i class="lar la-newspaper"></i>
+                                                        {{ $item->created_at }}
+                                                    </a>
+                                                </div>
+
+                                            </div>
+                                        </div>
+                                    @endforeach
+
+
+
+
+
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
     @else
         <p>No results found.</p>
     @endif
